5b1c393eb9c5c193d5cafea827b78fb358981423
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-09-01 23:17:28 -0500

    Uso de strings y colors, y uso de cadenas dinamicas

--- app/src/main/java/com/itiudc/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/itiudc/myfirstandroidapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.itiudc.myfirstandroidapp
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,11 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val res: Resources = resources
+
         val buttonGetNumber = findViewById<Button>(R.id.button_getNumber)
         val inputAge        = findViewById<EditText>(R.id.input_age)
         val inputName       = findViewById<EditText>(R.id.input_name)
         val showNumber      = findViewById<TextView>(R.id.show_number)
-        var temp = 0
+        val showPhrase      = findViewById<TextView>(R.id.subtitle)
+
+        var numberToShow = 0
 
         buttonGetNumber.setOnClickListener(){
             var numberAge:String = inputAge.text.toString()
@@ -30,18 +36,22 @@ class MainActivity : AppCompatActivity() {
 
             if(numberAge.toInt() < 10){
                 numberAge = (numberAge.toInt() * 7).toString()
-
                 if(numberAge.toInt() > 9){
-                    temp = operation(numberAge.toInt())
-                    showNumber.text = temp.toString()
+                    numberToShow = operation(numberAge.toInt())
                 }else{
-                    showNumber.text = numberAge
+                    numberToShow = numberAge.toInt()
                 }
             }else{
-                temp = operation(numberAge.toInt())
-                showNumber.text = temp.toString()
+                numberToShow = operation(numberAge.toInt())
             }
-            temp = 0
+
+            showPhrase.text = res.getString(R.string.main_subtitle, inputName.text.toString())
+            showNumber.text = res.getString(R.string.main_lucky_number, numberToShow.toString())
+
+            showPhrase.visibility = View.VISIBLE
+            showNumber.visibility = View.VISIBLE
+
+            numberToShow = 0
         }
     }
 
